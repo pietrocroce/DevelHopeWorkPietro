95540f3e4707ae41ea6f3435eb4c7ad37bde426e
Pietro_Croce <pcroce91@gmail.com>
2023-04-05 13:22:05 +0200

    Fatto esercizio 4 e pronto per la correzione

--- src/JavaBasic_4/main.java
+++ src/JavaBasic_4/main.java
@@ -2,28 +2,45 @@ package JavaBasic_4;
 
 public class main {
     public static void main(String[] args) {
-        exerciseone();
-        exercisetwo();
-        exercisethree();
+        exercise1();
+        exercise2();
+        exercise3();
     }
-    private static void exerciseone(){
-    int age = 32;
-    System.out.println("My age = " + age);
-    }
-    private static void exercisetwo(){
-    int age = 32;
-    char FirstLetterName = 'P';
-    System.out.println("My age is "+ age + " My initial Name is "+ FirstLetterName);
-    }
-    private static void exercisethree(){
-        //creo una variabile per sapere se è vero o no che ho mangiato
-        boolean haseatenlunch = false;
-        //variabile per conoscere il prezzo del pranzo
-        double lunchcost = 4.99;
-        //stampa del risultato se è vero che ho pranzato
-        System.out.println("has eaten lunch "+ haseatenlunch);
-        //stampa del risultato del costo del pranzo
-        System.out.println("the cost of lunch = "+ lunchcost);
+    /**
+     * 1: Store your age in an appropriate variable, print it out in
+     *    one line using the "+" in your print statement. Only use 1 print statement
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Write your code here
+        int age = 32;
+        System.out.println("My age = " + age);
     }
 
+    /**
+     * 2: Store your first name initial (i.e. Jane -> J) in a variable, your age in another variable.
+     *
+     *    And then print them out on the same line using the following idea
+     *
+     *    System.out.println("My Age=" + myAge +", my initial=" + myInitial);
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        //Write your code here
+        int age = 32;
+        char FirstLetterName = 'P';
+        System.out.println("My age is " + age + " My initial Name is " + FirstLetterName);
+    }
+
+    /**
+     * 3: Write comments above each line of code, describing what the line of code is doing
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        boolean hasEatenLunch = false;                //creazione di una variabile boolean con valore false
+        double lunchCost = 5.99;                      //creazione di una variabile double con valori decimali
+
+        System.out.println("Lunch cost=" + lunchCost); //stampa richiamando la variabile double
+        System.out.println("Has Eaten lunch=" + hasEatenLunch); // stampa richiamando la variabile boolean
+    }
 }
