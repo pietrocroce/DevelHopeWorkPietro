80204633069fb28df27221df83cae57e79e4f03c
Pietro_Croce <pcroce91@gmail.com>
2023-04-05 13:51:56 +0200

    Fatto esercizio 5 e pronto per la correzione

--- src/JavaBasic_5/main.java
+++ src/JavaBasic_5/main.java
@@ -1,56 +1,123 @@
 package JavaBasic_5;
 
 public class main {
+
     public static void main(String[] args) {
-        exerciseone();
-        exercisetwo();
-        exercisethree();
-        exerciseonebonus();
-        exercisetwobonus();
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4Bonus();
+        exercise5Bonus();
+        playAroundWithModulus();
     }
-    private static void exerciseone(){
-        int Number = 5;
-        int SumOfNumber = Number + 1;
-        int ResultWithMinus = Number - 1;
-        System.out.println(" The total of numbers is "+ SumOfNumber);
-        System.out.println("The result minus 1 is "+ ResultWithMinus);
-       }
-    private static void exercisetwo(){
-        int Redpens = 6;
-        int Blupens = 3;
-        int DivisionOfPens = Redpens / Blupens;
-        int MultiplicationOfPens = Redpens * Blupens;
-        System.out.println("The division is =" + DivisionOfPens);
-        System.out.println(" The moltiplication is "+ MultiplicationOfPens);
-        }
-    private static void exercisethree(){
-        int CelsiusOne = 20;
-        int Celsiustwo = 28;
-        double FahrenheitOne = CelsiusOne * 1.8 + 32;
-        double FahrenheitTwo = Celsiustwo * 1.8 + 32;
-       System.out.println(CelsiusOne + FahrenheitOne);
-        System.out.println(Celsiustwo + FahrenheitTwo);
-       }
-        //oppure
-        //    private static void exercisethree(){
-        //          int Celsius = 20;
-        //      double Fahrenheit = Celsius * 1.8 + 32;      da errore perchè la variabile Celsius è già stata usata
-        //     System.out.println(Celsius + Fahrenheit);      come fare ???
-        //    int Celsius = 28;
-        //    System.out.println(Celsius + Fahrenheit );
-        //}
-    private static void exerciseonebonus(){
+
+    /*
+            Remember the symbols you need to do maths are
+
+            Plus = +
+            Minus = -
+            Multiply or times = *
+            Divide = /
+     */
+
+    /**
+     * 1: Create an int variable, add 1 to the value and then print it. Use the + operator
+     * Then minus 1 from the same variable and print it.
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Write your code here
+        int SumOfNumber = 5 + 1;
+        int ResultWithMinus = 5 - 1;
+        System.out.println(" The total of numbers is " + SumOfNumber);
+        System.out.println("The result minus 1 is " + ResultWithMinus);
+    }
+
+    /**
+     * 2: Create a double variable, times by 2 and then print it.
+     * <p>
+     * Create another double variable, which is the previous variable divided by 2
+     * and then print it
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        // Write your code here
+        double Lunchprice = 6.99 * 2;
+        double Dinnerprice = 6.99 / 2;
+        System.out.println("My dinner cost = " + Dinnerprice);
+        System.out.println("My lunch cost = " + Lunchprice);
+    }
+
+    /**
+     * 3: We are going to convert the temperature from celsius to fahrenheit
+     * <p>
+     * Create a double variable called celsius
+     * <p>
+     * Create a new variable called fahrenheit
+     * <p>
+     * The variable fahrenheit should be set as
+     * celsius times by 1.8 plus 32
+     * <p>
+     * Print out celsius and fahrenheit with 2 print statements
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        // Write your code here
+        double Celsius = 20;
+        double Fahrenheit = Celsius * 1.8 + 32.0;
+        System.out.println(Celsius);
+        System.out.println(Fahrenheit);
+    }
+
+    /**
+     * 4: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age with a starting value of 10.
+     * <p>
+     * Only using the '++' operator increase this value to 15
+     */
+    private static void exercise4Bonus() {
+        System.out.println("\nExercise 4:");
+        // Write your code here
         int Age = 10;
-        var IncreaseAge = Age +=5;
-        System.out.println(IncreaseAge);
+        Age += 5;
+        System.out.println(Age);
     }
-    private static void exercisetwobonus(){
-    int Age = 50;
-    int newAge = Age -= 50;
-    int Agetwo = 52;
-    int decreaseAge = Agetwo -=2;
-    int decreaseAgainAge = decreaseAge-=50;
-    System.out.println(newAge);
-    System.out.print(decreaseAgainAge);
+
+    /**
+     * 5: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age set as 50
+     * <p>
+     * Using the -= operator change this value from 50 to 0 in one line of code,
+     * <p>
+     * Underneath create another int called secondAge set as 50
+     * <p>
+     * Change this value from 50 to 0 in 3 lines of code
+     */
+    private static void exercise5Bonus() {
+        System.out.println("\nExercise 5:");
+        // Write your code here
+        int Age = 50 ;
+        Age -= 50;
+        System.out.println(Age);
+        int secondAge = 50;
+        secondAge -= 20;
+        secondAge -=20;
+        secondAge -=10;
+        System.out.println(secondAge);
+    }
+
+    /**
+     * This is completely optional, but if you guys want to play around with
+     * modulus to get a better understanding. Simply change the code below
+     */
+    private static void playAroundWithModulus() {
+        System.out.println("\nModulus testing!:");
+
+        int value = 5;   // <--- change this value
+        int modulus = 4; // <--- and this value to see the results of modulus
+        double ValueRest =  value % modulus;
+        System.out.println("Modulus calculation, " + ValueRest);
     }
 }
