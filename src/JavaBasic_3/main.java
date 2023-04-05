7b7aa83ffcbd487ade52804c4b86a566ef0f3499
Pietro_Croce <pcroce91@gmail.com>
2023-04-05 13:06:08 +0200

    Fatto esercizio 3 e pronto per la correzione

--- src/JavaBasic_3/main.java
+++ src/JavaBasic_3/main.java
@@ -2,29 +2,60 @@ package JavaBasic_3;
 
 public class main {
     public static void main(String[] args) {
-    exerciseone();
-    exercisetwo();
-    exercisethree();
-}
-   private static void exerciseone(){
+        exercise1();
+        exercise2();
+        exercise3();
+    }
 
-    System.out.println("P C");
-}
-    private static void exercisetwo() {
-        System.out.println(32);
-        System.out.println("hasHadMyLunchToday = TRUE");
+    /**
+     * 1: print out your initials using System.out.print and then a char literal,
+     * i.e. 'a', 'b', 'c'. You will need multiple print statements
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Write your code here
+        char name = 'P';
+        char surname = 'C';
+        System.out.print("P C"+"."+ name +"-"+ surname);
+    }
+
+    /**
+     * 2: Print out your age as an int literal, i.e. 28, then print whether or not you've
+     * had lunch today as a boolean literal i.e. true, false, then print the price of
+     * your lunch as a double, i.e. 4.99
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        // Write your code here
+        int age = 28;
+        boolean hasHadMyLunchToday = true;
+        double MylunchPrice = 4.99;
+        System.out.println(28);
+        System.out.println("true ");
         System.out.println(4.99);
     }
-    private static void exercisethree() {
+
+    /**
+     * 3: Complete exercise 2, but store the values in a variable.
+     * And then print out the variable.
+     *
+     * i.e.
+     * char favouriteLetter = 'g';
+     * System.out.print("My favourite letter=");
+     * System.out.println(favouriteLetter)
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        // Write your code here
         char FirstLetter = 'P';
         char SecondLetter = 'C';
-        System.out.print(FirstLetter);
-        System.out.println(SecondLetter);
+        System.out.print("My name start with letter "+ FirstLetter);
+        System.out.println("My surname start with letter "+ SecondLetter);
         int MyAge = 32;
-        System.out.println(MyAge);
+        System.out.println("My age is " + MyAge);
         boolean hasHadMyLunchToday = false;
-        System.out.println(hasHadMyLunchToday);
+        System.out.println(" I have eaten today " + hasHadMyLunchToday);
         double LunchPrice = 4.99;
-        System.out.println(LunchPrice);
+        System.out.println("My lunch cost " + LunchPrice);
     }
 }
